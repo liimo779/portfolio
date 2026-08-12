a017e4acc659ff323cf5178b9e18006ea9ff12dc
liimo779 <liimoo.cd@gmail.com>
2026-08-12 12:13:30 +0300

    fix4

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use("/api", portfolioRoutes);
 app.use("/api/admin", adminRoutes);
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
