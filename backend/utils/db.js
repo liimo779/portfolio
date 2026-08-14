28a7969c5b2a125ba07bca6645ccf278fab59384
liimo779 <liimoo.cd@gmail.com>
2026-08-14 06:24:40 +0300

    fix

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,33 +1,3 @@
 const db = require("../config/db");
 
-const convertPlaceholders = (sql) => {
-  let index = 0;
-  return sql.replace(/\?/g, () => {
-    index++;
-    return `$${index}`;
-  });
-};
-
-const run = async (sql, params = []) => {
-  const query = convertPlaceholders(sql);
-  const result = await db.query(query, params);
-  return result; // يرجع كائن نتيجة pg بالكامل (يشمل rows و rowCount)
-};
-
-const get = async (sql, params = []) => {
-  const query = convertPlaceholders(sql);
-  const result = await db.query(query, params);
-  return result.rows[0];
-};
-
-const all = async (sql, params = []) => {
-  const query = convertPlaceholders(sql);
-  const result = await db.query(query, params);
-  return result.rows;
-};
-
-module.exports = {
-  run,
-  get,
-  all,
-};
+module.exports = db;
